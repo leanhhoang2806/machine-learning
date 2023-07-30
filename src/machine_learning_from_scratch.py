c5432d977b381fab31cb651c9d948ab55d4a4a4b
hoang <leanhhoang28061988@gmail.com>
2023-07-30 00:56:10 -0500

    change model

--- src/machine_learning_from_scratch.py
+++ src/machine_learning_from_scratch.py
@@ -1,44 +1,41 @@
-import os
 import tensorflow as tf
 import json
+import os
 
+BUFFER_SIZE = 10000
+BATCH_SIZE = 4
 
-physical_devices = tf.config.list_physical_devices('GPU')
-if physical_devices:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+def build_and_compile_cnn_model():
+  model = tf.keras.Sequential([
+      tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
+      tf.keras.layers.MaxPooling2D(),
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(64, activation='relu'),
+      tf.keras.layers.Dense(10, activation='softmax')
+  ])
+  model.compile(
+      loss=tf.keras.losses.sparse_categorical_crossentropy,
+      optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
+      metrics=['accuracy'])
+  return model
 
-def build_model():
-    return tf.keras.Sequential([
-        tf.keras.layers.Dense(64, activation='relu', input_shape=(784,)),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(10, activation='softmax')
-    ])
-
-def train_model():
-
-    cluster_spec = {
-        "worker": [
-            "{}:2222".format('192.168.1.100'),
-            "{}:2222".format('192.168.1.101'),
-            ]
-    }
-
-    os.environ["TF_CONFIG"] = json.dumps({
-        "cluster": cluster_spec,
-        "task": {"type": "worker", "index": 0}
+def train_task(index):
+    os.environ['TF_CONFIG'] = json.dumps({
+        'cluster': {
+            'worker': ["192.168.1.101:2222","192.168.1.100:2222"],
+        },
+        'task': {'type': 'worker', 'index': index},
     })
-
-    strategy = tf.distribute.MultiWorkerMirroredStrategy()
-
+    strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
     with strategy.scope():
         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-        x_train, x_test = x_train / 255.0, x_test / 255.0
-        model = build_model()
-        model.compile(optimizer='adam',
-                    loss='sparse_categorical_crossentropy',
-                    metrics=['accuracy'])
+        x_train = x_train.reshape(-1, 28, 28, 1) / 255.0
+        x_test = x_test.reshape(-1, 28, 28, 1) / 255.0
+        multi_worker_model = build_and_compile_cnn_model()
 
-    model.fit(x_train, y_train, epochs=10, batch_size=64)
+    multi_worker_model.fit(x_train, y_train, epochs=3)
 
-if __name__ == "__main__":
-    train_model()
+# runs on ip1
+train_task(0)
+# runs on ip2
+# train_task(1)
